66869924bb597777626ed2a7f36a2dc509bf330a
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2024-10-14 17:44:06 -0400

    Initial commit

---
+++ Casks/nuke.rb
@@ -0,0 +1,27 @@
+cask "nuke" do
+    version "15.1v3"
+
+    on_intel do
+        sha256 "afefb1cce28834eeb3d1f9bb81cdd6cbf5cf4a7637c5bb50ba79436fcc18b030"
+        url "https://www.foundry.com/products/download_product?file=Nuke#{version}-mac-x86_64.dmg"
+    end
+    
+    on_arm do
+        sha256 "04ba988f263a07f526fe34b573c15bb7e2500a92f8f8da86c98dec686d9b9a82"
+        url "https://www.foundry.com/products/download_product?file=Nuke#{version}-mac-arm64.dmg"
+    end 
+
+    name "Nuke"
+    desc "Node-based compositing application"
+    homepage "https://www.foundry.com/"
+  
+    depends_on macos: ">= :monterey"
+
+    app "Nuke#{version}"
+
+    zap trash: [
+      "/Applications/Nuke#{version}",
+      "~/Library/Saved Application State/uk.co.thefoundry.Nuke#{version}.savedState",
+      "~/Library/Preferences/uk.co.thefoundry.Nuke#{version}.plist",
+    ]
+end
--- README.md
+++ README.md
@@ -1,16 +1,29 @@
-# Sisoe24 Nuke
+# Homebrew tap for VFX software
+
+This repository contains formulae for VFX software that are not available in the main Homebrew tap.
+
+## Included software
+
+- [Nuke](https://www.foundry.com/products/nuke)
 
 ## How do I install these formulae?
 
-`brew install sisoe24/nuke/<formula>`
+`brew install sisoe24/vfx/<formula>`
 
-Or `brew tap sisoe24/nuke` and then `brew install <formula>`.
+Or `brew tap sisoe24/vfx` and then `brew install <formula>`.
 
 Or, in a [`brew bundle`](https://github.com/Homebrew/homebrew-bundle) `Brewfile`:
 
 ```ruby
-tap "sisoe24/nuke"
+tap "sisoe24/vfx"
 brew "<formula>"
+```
+
+## Example
+
+```bash
+brew tap sisoe24/vfx
+brew install nuke
 ```
 
 ## Documentation
